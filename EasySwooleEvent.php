f3f0b5e17b32e0b30ee12da42bb07ef61057d4dc
suhanyujie <suhanyujie@qq.com>
2019-02-25 08:28:23 +0800

    add tcp client

--- EasySwooleEvent.php
+++ EasySwooleEvent.php
@@ -30,6 +30,32 @@ class EasySwooleEvent implements Event
         // 热重载
         $swooleServer = ServerManager::getInstance()->getSwooleServer();
         $swooleServer->addProcess((new HotReload("HotReload",["disableInotify"=>false]))->getProcess());
+        //连接dom解析器
+//        $client = new \swoole_client(SWOOLE_SOCK_TCP);
+//        if ($client->connect('127.0.0.1',8001,2)) {
+//            echo "dom解析服务连接不上！\n";
+//            return;
+//        }
+//        for ($i=0;$i<10;$i++) {
+//            $con = "<h1 id='list'>hello world<dd><a href='http://www.baidu.com'>test link</a></dd></h1>";
+//            $ok = $client->send("11111111");
+//            if (!$ok) {
+//                echo "向dom解析服务发送异常！\n";
+//                return;
+//            }
+//            sleep(2);
+//        }
+//        $data = $client->recv();
+//        var_dump($data);
+        $fp = fsockopen("tcp://127.0.0.1",8001,$errno,$error,2);
+        var_dump($fp);
+        echo "{$error}";
+        return;
+        fwrite($fp, "11111",5);
+        while (!feof($fp)) {
+            echo fgets($fp, 128);
+        }
+        fclose($fp);
     }
 
     public static function onRequest(Request $request, Response $response): bool
